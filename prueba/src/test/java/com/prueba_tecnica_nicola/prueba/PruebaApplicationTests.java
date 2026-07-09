dfe512cda1a74490d231a2b75e87a9abae6b9a79
Nicola Di Sabato González <nicoesp1000@hotmail.com>
2026-07-09 14:57:30 +0200

    feature/creacion del proyecto

---
+++ prueba/src/test/java/com/prueba_tecnica_nicola/prueba/PruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba_tecnica_nicola.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
